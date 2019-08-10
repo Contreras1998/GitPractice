d8a18fee052c80ce13d60a22f46b173d452c7e50
contreras1998 <oandres1411@hotmail.com>
2019-08-10 17:35:50 +0100

    First

---
+++ GitPractice/Homepage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPractice
+{
+    public class Homepage
+    {
+        public Homepage(string header, string userInput)
+        {
+            Header = header;
+            UserInput = userInput;
+        }
+        public string Header { get; set; }
+        public string UserInput { get; set; }
+
+        public void GetProperties()
+        {
+            Console.WriteLine(Header,UserInput);
+        }
+    }
+}
---
+++ GitPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var homePage = new Homepage("Headphones", "Huawey Freebudds");
+            homePage.GetProperties();
+        }
+    }
+}
